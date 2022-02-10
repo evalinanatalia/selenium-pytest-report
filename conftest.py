f95982bc4c96d5d12bc31e4cc97cf11aced609cd
vaaa <evalina.simangunsong@ottodigital.id>
2022-02-10 09:07:56 +0700

    take SS for failed test case

--- conftest.py
+++ conftest.py
@@ -15,27 +15,27 @@ def pytest_runtest_makereport(item):
     report = outcome.get_result()
     extra = getattr(report, 'extra', [])
 
-    # if report.when == 'call' or report.when == "setup":
-    #     xfail = hasattr(report, 'wasxfail')
-    #     if (report.skipped and xfail) or (report.failed and not xfail):
-    #         file_name = report.nodeid.replace("::", "_")+".png"
-    #         screen_img = _capture_screenshot()
-    #         if file_name:
-    #             # html = '<div><img src="data:image/png;base64,%s" alt="screenshot" style="width:600px;height:300px;" ' \
-    #             #        'onclick="window.open(this.src)" align="right"/></div>' % screen_img
-    #             # extra.append(pytest_html.extras.html(html))
-    #             now = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
-    #             nama_gambar = 'gambar-%s.png' % now
-    #             gambar = driver.get_screenshot_as_file(nama_gambar)
-    #             ofile = os.path.join(os.getcwd(), nama_gambar)
+    if report.when == 'call' or report.when == "setup":
+        xfail = hasattr(report, 'wasxfail')
+        if (report.skipped and xfail) or (report.failed and not xfail):
+            file_name = report.nodeid.replace("::", "_")+".png"
+            screen_img = _capture_screenshot()
+            if file_name:
+                # html = '<div><img src="data:image/png;base64,%s" alt="screenshot" style="width:600px;height:300px;" ' \
+                #        'onclick="window.open(this.src)" align="right"/></div>' % screen_img
+                # extra.append(pytest_html.extras.html(html))
+                now = datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')
+                nama_gambar = 'gambar-%s.png' % now
+                gambar = driver.get_screenshot_as_file(nama_gambar)
+                ofile = os.path.join(os.getcwd(), nama_gambar)
 
-    #             html = '<div><img src="%s" alt="screenshot" style="width:600px;height:300px;" ' \
-    #                    'onclick="window.open(this.src)" align="right"/></div>' % ofile
-    #             extra.append(pytest_html.extras.html(html))
+                html = '<div><img src="%s" alt="screenshot" style="width:600px;height:300px;" ' \
+                       'onclick="window.open(this.src)" align="right"/></div>' % ofile
+                extra.append(pytest_html.extras.html(html))
 
-    #             send_email = _send_email_report()
+                send_email = _send_email_report()
 
-    #     report.extra = extra
+        report.extra = extra
 
 def _capture_screenshot():
     '''
